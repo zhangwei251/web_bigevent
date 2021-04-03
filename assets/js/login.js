9a86ada4ed3b90478e401ac977b43d234d90c915
zhangwei251 <itheima@itcast.cn>
2021-04-04 00:27:16 +0800

    提交登录链接

---
+++ assets/js/login.js
@@ -0,0 +1,14 @@
+$(function(){
+    //点击"去注册账户"的链接
+   $('#link_reg').on('click',function(){
+    $('.login-box').hide();
+    $('.reg-box').show();
+   });
+
+
+    //点击"去登录"的链接
+    $('#link_login').on('click',function(){
+        $('.login-box').show();
+        $('.reg-box').hide();
+    });
+})
