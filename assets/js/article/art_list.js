186eb17b4bbf4bbe90f276f306e5b34a8082f466
zhangwei251 <itheima@itcast.cn>
2021-04-12 23:53:02 +0800

    文章列表增加分页功能

--- assets/js/article/art_list.js
+++ assets/js/article/art_list.js
@@ -49,7 +49,7 @@ $(function() {
         var htmlStr = template('tpl-table', res)
         $('tbody').html(htmlStr)
         // 调用渲染分页的方法
-        renderPage(res.total)
+        renderPage(res.total);
       }
     })
   }
@@ -84,71 +84,38 @@ $(function() {
     // 根据最新的筛选条件，重新渲染表格的数据
     initTable()
   })
-
-  // 定义渲染分页的方法
-  function renderPage(total) {
-    // 调用 laypage.render() 方法来渲染分页的结构
+  function renderPage(total){
+ 
     laypage.render({
-      elem: 'pageBox', // 分页容器的 Id
-      count: total, // 总数据条数
-      limit: q.pagesize, // 每页显示几条数据
-      curr: q.pagenum, // 设置默认被选中的分页
-      layout: ['count', 'limit', 'prev', 'page', 'next', 'skip'],
-      limits: [2, 3, 5, 10],
-      // 分页发生切换的时候，触发 jump 回调
-      // 触发 jump 回调的方式有两种：
-      // 1. 点击页码的时候，会触发 jump 回调
-      // 2. 只要调用了 laypage.render() 方法，就会触发 jump 回调
-      jump: function(obj, first) {
-        // 可以通过 first 的值，来判断是通过哪种方式，触发的 jump 回调
-        // 如果 first 的值为 true，证明是方式2触发的
-        // 否则就是方式1触发的
-        console.log(first)
-        console.log(obj.curr)
-        // 把最新的页码值，赋值到 q 这个查询参数对象中
-        q.pagenum = obj.curr
-        // 把最新的条目数，赋值到 q 这个查询参数对象的 pagesize 属性中
-        q.pagesize = obj.limit
-        // 根据最新的 q 获取对应的数据列表，并渲染表格
-        // initTable()
-        if (!first) {
-          initTable()
-        }
+    elem: 'pageBox', //注意，这里的 test1 是 ID，不用加 # 号
+    count: total, //数据总数，从服务端得到
+    limit : q.pagesize, //每页显示几条信息
+    curr : q.pagenum, //默认选中第几页数据
+    layout : ['count','limit','prev', 'page', 'next','skip'],
+    limits : [2,3,5,10],  
+    /* 
+      分页发生切换，会触发jump回调
+      触发jump的方式有两种
+      1.点击页码的时候，会触发jump回调
+      2.只要调用了lyaer.page()方法就会触发jump回调
+    */
+    jump : function(obj,first){
+      //可以通过first的值，来判断是哪种方试触发的jump回调
+      //如果first的值为true 是方式一处罚的否则是方式2
+      // console.log(first);
+      // console.log(obj.curr);
+      console.log(obj.limit);
+       //将做新的页码值赋值到q对象中再调用initTable函数渲染页面
+      q.pagenum = obj.curr;
+      //把最新的条目数赋值到q 这个查询参数对象的pageSize属性中
+      q.pagesize = obj.limit;
+      // 根据据最新的q{}获取对应的数据列表，渲染数据
+      if(!first){
+        initTable();
       }
+     
+    }
     })
   }
 
-  // 通过代理的形式，为删除按钮绑定点击事件处理函数
-  $('tbody').on('click', '.btn-delete', function() {
-    // 获取删除按钮的个数
-    var len = $('.btn-delete').length
-    console.log(len)
-    // 获取到文章的 id
-    var id = $(this).attr('data-id')
-    // 询问用户是否要删除数据
-    layer.confirm('确认删除?', { icon: 3, title: '提示' }, function(index) {
-      $.ajax({
-        method: 'GET',
-        url: '/my/article/delete/' + id,
-        success: function(res) {
-          if (res.status !== 0) {
-            return layer.msg('删除文章失败！')
-          }
-          layer.msg('删除文章成功！')
-          // 当数据删除完成后，需要判断当前这一页中，是否还有剩余的数据
-          // 如果没有剩余的数据了,则让页码值 -1 之后,
-          // 再重新调用 initTable 方法
-          // 4
-          if (len === 1) {
-            // 如果 len 的值等于1，证明删除完毕之后，页面上就没有任何数据了
-            // 页码值最小必须是 1
-            q.pagenum = q.pagenum === 1 ? 1 : q.pagenum - 1
-          }
-          initTable()
-        }
-      })
-
-      layer.close(index)
-    })
-  })
 })
